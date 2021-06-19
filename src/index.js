ffee4d0a666436db2b9e854c2eababef56a76735
wumo1016 <760478279@qq.com>
2021-06-19 17:15:23 +0800

    init

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// let title = require('./title')
-// let user = require('./user.json')
-// let test = require('./test.txt')
-// console.log(title);
-// console.log(user);
-// console.log(test.default);
-
-console.log(process.env.NODE_ENV);

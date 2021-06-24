e95edcedaeabb0af25435824564fc9ded105a0e7
wu.yibo <88888888>
2021-06-24 18:23:42 +0800

    update(entry)

--- 1.usage/src/index.js
+++ 1.usage/src/index.js
@@ -1,5 +1,1 @@
-const a = 12345
-const b = 456
-console.log(a);
-console.log(b);
-console.log(process.env.NODE_ENV);
+console.log('index');
--- 1.usage/webpack.config.js
+++ 1.usage/webpack.config.js
@@ -1,9 +1,8 @@
 const path = require('path')
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  context: path.resolve(__dirname, 'src'),
+  mode: process.env.NODE_ENV === 'development' ? 'development' : 'production',
   entry: {
-    index: './index.js'
+    index: './src/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),

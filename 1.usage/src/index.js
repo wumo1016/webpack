4e332d2da01e6f441d425f765b1a217115da0e7b
wumo1016 <760478279@qq.com>
2021-06-28 20:15:33 +0800

    update(支持图片)

--- 1.usage/src/index.js
+++ 1.usage/src/index.js
@@ -1,2 +1,3 @@
-import './index.css'
+import './index.scss'
 console.log(1)
+
--- 1.usage/webpack.config.js
+++ 1.usage/webpack.config.js
@@ -17,11 +17,32 @@ module.exports = {
         test: /\.txt$/,
         loader: "raw-loader"
       },
-      { // 需要安装包 style-loader css-loader
+      {
         test: /\.css$/,
         use: [
           'style-loader', // 可以将css插入到dom中
           'css-loader', // 处理 @import和url()
+        ]
+      },
+      {
+        test: /\.(scss|sass)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif|svg)/,
+        use: [
+          {
+            loader: 'url-loader', // 功能大于file-loader 比它多limit参数
+            options: {
+              esModule: false, // require之后不用使用default取值
+              name: '[hash:10].[ext]', // 文件名 [hash:]取10位hash值 [ext]原来的扩展名
+              limit: 10 * 1024 // 8k 小于8k就是转成base64字符串
+            }
+          }
         ]
       }
     ],

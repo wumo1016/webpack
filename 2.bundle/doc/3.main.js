8a68f120053c2957978947d9255ff0cee1b0a365
wumo1016 <760478279@qq.com>
2021-07-03 15:56:15 +0800

    update(es=>es)

---
+++ 2.bundle/doc/3.main.js
@@ -0,0 +1,36 @@
+// esmodule 加载 esmodule
+var modules = {
+  './src/index.js': (module, exports, require) => {
+    require.r(exports)
+    require.d(exports, {
+      'default': () => ('title'),
+      'age': () => ('age')
+    })
+  }
+}
+
+function require(moduleId) {
+  let module = {
+    exports: {}
+  }
+  modules[moduleId](module, module.exports, require)
+  return module.exports
+}
+
+require.d = (exports, definiton) => {
+  for (const key in definiton) {
+    Object.defineProperty(exports, key, {
+      enumerable: true,
+      get: definiton[key]
+    })
+  }
+}
+
+require.r = (exports) => {
+  Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' })
+  Object.defineProperty(exports, '_esModule', { value: true })
+}
+
+let _title_ = require('./src/index.js')
+console.log(_title_.default);
+console.log(_title_.age);

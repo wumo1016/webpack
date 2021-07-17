6c8495a822817567320485b294a011b853cc0205
wumo1016 <760478279@qq.com>
2021-07-17 15:33:32 +0800

    init

---
+++ 6.loader/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const resolve = paths => path.resolve(__dirname, paths)
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: './src/index.js',
+  output: {
+    path: resolve('dist'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve('./src/index.html'),
+      filename: 'index.html'
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/*']
+    }),
+  ]
+}

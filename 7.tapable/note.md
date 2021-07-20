54e36e487a15a666af753cfa4911a1f95ebd8d48
wumo1016 <760478279@qq.com>
2021-07-21 07:16:51 +0800

    feat(SyncWaterfallHook)

--- 7.tapable/note.md
+++ 7.tapable/note.md
@@ -3,3 +3,6 @@
 - SyncHook
 - SyncBailHook
   - 一旦中间有一个注册的函数返回的不是undefined 后面注册的函数不再执行
+- SyncWaterfallHook
+  - 前一个函数返回的结果 会作为后一个函数的参数
+  - 如果后一个函数没有返回值 会拿前面一个函数的返回值作为参数
---
+++ 7.tapable/src/3.SyncWaterfallHook.js
@@ -0,0 +1,29 @@
+const { SyncWaterfallHook } = require('tapable')
+
+const hook = new SyncWaterfallHook(['a'])
+
+hook.tap('1', (name) => {
+  console.log(1, name)
+})
+
+hook.tap('2', (name) => {
+  console.log(2, name)
+  return '2'
+})
+
+hook.tap('3', (name) => {
+  console.log(3, name)
+})
+
+hook.tap('4', (name) => {
+  console.log(4, name)
+})
+
+hook.call('wyb')
+
+/* 
+1 wyb
+2 wyb
+3 2
+4 2
+*/

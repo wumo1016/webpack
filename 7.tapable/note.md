5da606da2bf1679ded34d1c9611b634783aa574e
wumo1016 <760478279@qq.com>
2021-07-22 07:05:33 +0800

    SyncLoopHook

--- 7.tapable/note.md
+++ 7.tapable/note.md
@@ -1,8 +1,12 @@
 ## tapable
 
 - SyncHook
+  - 从前往后触发
 - SyncBailHook
   - 一旦中间有一个注册的函数返回的不是undefined 后面注册的函数不再执行
 - SyncWaterfallHook
   - 前一个函数返回的结果 会作为后一个函数的参数
   - 如果后一个函数没有返回值 会拿前面一个函数的返回值作为参数
+- SyncLoopHook
+  - 不停的循环执行函数 直到函数返回的结果都是undefined为止
+  - 每次循环都是从头开始
---
+++ 7.tapable/src/4.SyncLoopHook.js
@@ -0,0 +1,56 @@
+const { SyncLoopHook } = require('tapable')
+
+const hook = new SyncLoopHook(['a'])
+
+let count1 = 0
+let count2 = 0
+let count3 = 0
+
+hook.tap('1', (name) => {
+  console.log(1, count1)
+  if (++count1 === 1) {
+    count1 = 0
+    return
+  }
+  return true
+})
+
+hook.tap('2', (name) => {
+  console.log(2, count2)
+  if (++count2 === 2) {
+    count2 = 0
+    return
+  }
+  return true
+})
+
+hook.tap('3', (name) => {
+  console.log(3, count3)
+  if (++count3 === 3) {
+    count3 = 0
+    return
+  }
+  return true
+})
+
+hook.call('wyb')
+
+/* 
+1 0
+2 0
+1 0
+2 1
+3 0
+
+1 0
+2 0
+1 0
+2 1
+3 1
+
+1 0
+2 0
+1 0
+2 1
+3 2
+*/

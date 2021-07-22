947eeddf5dd5b7da74fd0192a997a32a16717f49
wumo1016 <760478279@qq.com>
2021-07-23 07:05:08 +0800

    feat(AsyncSeriesHook)

---
+++ 7.tapable/src/7.AsyncSeriesHook.js
@@ -0,0 +1,79 @@
+const { AsyncSeriesHook } = require('tapable')
+
+const hook = new AsyncSeriesHook(['a'])
+
+/* ------------------------ 第一种写法 --------------------------- */
+/* console.time('async')
+hook.tapAsync('1', (name, cb) => {
+  setTimeout(() => {
+    console.log(1, name)
+    cb()
+  }, 1000)
+})
+
+hook.tapAsync('2', (name, cb) => {
+  setTimeout(() => {
+    console.log(2, name)
+    cb()
+  }, 2000)
+})
+
+hook.tapAsync('3', (name, cb) => {
+  setTimeout(() => {
+    console.log(3, name)
+    cb()
+  }, 3000)
+})
+
+hook.callAsync('wyb', err => {
+  console.timeEnd('async')
+  console.log(err)
+}) */
+
+/* 
+1 wyb
+2 wyb
+3 wyb
+async: 6.027s
+undefined
+*/
+
+/* ------------------------ 第三种写法 --------------------------- */
+console.time('async')
+hook.tapPromise('1', name => {
+  return new Promise((r, j) => {
+    setTimeout(() => {
+      console.log(1, name)
+      r()
+    }, 1000)
+  })
+})
+
+hook.tapPromise('2', (name, cb) => {
+  return new Promise((r, j) => {
+    setTimeout(() => {
+      console.log(2, name)
+      r()
+    }, 2000)
+  })
+})
+
+hook.tapPromise('3', (name, cb) => {
+  return new Promise((r, j) => {
+    setTimeout(() => {
+      console.log(3, name)
+      r()
+    }, 3000)
+  })
+})
+
+hook.promise('wyb').then(res => {
+  console.timeEnd('async')
+})
+
+/* 
+1 wyb
+2 wyb
+3 wyb
+async: 6.032s
+*/

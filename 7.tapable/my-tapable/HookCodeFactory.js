d591ccdbf21d176c18bef01d351dac0e79c07d26
wumo1016 <760478279@qq.com>
2021-07-25 21:47:00 +0800

    chore(my-tapable)

--- 7.tapable/my-tapable/HookCodeFactory.js
+++ 7.tapable/my-tapable/HookCodeFactory.js
@@ -1,9 +1,39 @@
 class HookCodeFactory {
-  setup() {}
+  setup(hookInstance, options) {
+    hookInstance._x = options.taps.map(tap => tap.fn)
+  }
 
-  create() {}
+  create(options) {
+    this.init(options)
+    let fn
+    switch (options.type) {
+      case 'sync':
+        fn = new Function(this.args(options), this.header() + this.content())
+        break
+      default:
+        break
+    }
+  }
 
-  callTapsSeries() {}
+  init() {
+    this.options = options
+  }
+
+  args(options) {
+    // options =>{ taps, type, args}
+    let args = options.args || []
+    return args.join(',')
+  }
+
+  header() {
+    return 'var _x = this._x'
+  }
+
+  callTapsSeries() {
+    const taps = this.options.taps
+    if (taps.length < 1) return ''
+    let code = ''
+  }
 }
 
 module.exports = HookCodeFactory

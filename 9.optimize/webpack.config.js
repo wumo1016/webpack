394629969143d4a7dc3cc4e8ea59f1c197b63acb
wumo1016 <760478279@qq.com>
2021-08-13 07:24:12 +0800

    chore(optimize)

---
+++ 9.optimize/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const resolve = paths => path.resolve(__dirname, paths)
+
+module.exports = {
+  mode: 'development',
+  devtool: false,
+  entry: './src/index.js',
+  output: {
+    path: resolve('dist'),
+    filename: '[name].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+}

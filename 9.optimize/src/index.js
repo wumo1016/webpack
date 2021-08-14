d35d1a499809ad589570887a08933a76ac4f5cb0
wumo1016 <760478279@qq.com>
2021-08-14 11:00:52 +0800

    chore(optimize)

--- 9.optimize/src/index.js
+++ 9.optimize/src/index.js
@@ -1,2 +1,8 @@
-const title = require('title')
+// import 'bootstrap'
+
+const title = require('./title')
 console.log(title)
+
+// 默认查找文件
+// import test from '../my-modules/test'
+// console.log(test);
--- 9.optimize/webpack.config.js
+++ 9.optimize/webpack.config.js
@@ -2,17 +2,40 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const resolve = paths => path.resolve(__dirname, paths)
 
+const bootstrap = resolve('node_modules/bootstrap/dist/css/bootstrap.min.css')
+
 module.exports = {
   mode: 'development',
   devtool: false,
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    vendor: ['lodash'],
+  },
   output: {
     path: resolve('dist'),
     filename: '[name].js',
   },
+  resolve: {
+    // 可以不添加后缀 按照这个顺序查找
+    extensions: ['.js', '.json'],
+    // 依赖别名
+    alias: {
+      bootstrap,
+    },
+    // 指定依赖的查找位置 如果有的依赖不在node_modules中 可以添加
+    modules: ['node_modules', 'my-modules'],
+    // 打包target 为web|webworker时 默认值为 ['browser', 'module', 'main']
+    // 为其他值时 默认值为 ['module', 'main']
+    mainFields: ['root', 'browser', 'module', 'main'],
+    // 查找文件夹下面默认的文件 默认值时 ['index']
+    mainFiles: ['main', 'index'],
+  },
+  // 如何查找loader的配置
+  resolveLoader: {},
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      chunks: ['main'], // 表示需要插入的脚本
     }),
   ],
 }
